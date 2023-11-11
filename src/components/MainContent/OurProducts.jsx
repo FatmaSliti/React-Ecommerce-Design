c51b5e64e6555bf10abc05b2c8e0204b23e7d696
FatmaSliti <fatmasliti289@gmail.com>
2023-11-11 08:55:27 +0100

    responsiveness

--- src/components/MainContent/OurProducts.jsx
+++ src/components/MainContent/OurProducts.jsx
@@ -6,7 +6,7 @@ import Image from '../../assets/Images.png';
 const OurProducts = () => {
     return (
         <>
-            <div className={styles.header}>
+            <div className={styles.products}>
                 <p>Our Products</p>
                 <p>View All</p>
             </div>
@@ -15,7 +15,7 @@ const OurProducts = () => {
                     <Card image={Image} title='Monitor 231' price='$986' />
                     <Card image={Image} title='Headphone NM12' price='$678' />
                     <Card image={Image} title='Mouse V1' price='$543' />
-                    <Card image={Image} title='Keyboard K2' price='$299' />
+                    {/* <Card image={Image} title='Keyboard K2' price='$299' /> */}
                     {/* <Card image={Image} title='Keyboard K2' price='$299' />
                     <Card image={Image} title='Keyboard K2' price='$299' /> */}
                 </div>
